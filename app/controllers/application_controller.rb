9437e0f079dbbd45fea292dedc103644eab6113d
BlackDiscGH <avinashjham@Avinashs-MacBook-Pro.local>
2015-11-13 15:52:19 -0500

    Added User name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception  #what is this????
 
-  def ensure_logged_in
+  def ensure_logged_in       
     unless current_user
       flash[:alert] = "Please log in"
       redirect_to new_session_path
@@ -12,10 +12,16 @@ class ApplicationController < ActionController::Base
 
 	private
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+  	def current_user
+    	@current_user ||= User.find(session[:user_id]) if session[:user_id]    # @current_user is an instance variable. Defined in Application Controller. so it will be available throughout the instance.
+  	                                                                         #http://www.rubyinside.com/what-rubys-double-pipe-or-equals-really-does-5488.html
+    #binding.pry
+    end
 
-  helper_method :current_user
+    # The method helper_method is to explicitly share some methods defined in the controller to make them available 
+    # for the view. This is used for any method that you need to access from both controllers and helpers/views 
+    # (standard helper methods are not available in controllers).
+     
+  	helper_method :current_user
 
-end
+	end
